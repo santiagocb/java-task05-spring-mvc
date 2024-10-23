229f1d8fe45c11ea62c0f140380a075cdc8d98be
Santiago <santiagocadavidbte@gmail.com>
2024-10-22 22:43:35 -0500

    Initial commit

---
+++ src/main/java/com/ticketland/java_task05_spring_mvc/JavaTask05SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ticketland.java_task05_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTask05SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTask05SpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketland/java_task05_spring_mvc/JavaTask05SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketland.java_task05_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTask05SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
